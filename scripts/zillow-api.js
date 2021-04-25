0f45da461f70bd554ed5516b9db3bb8c09401e2b
Francisco Martinez <panchetz7@gmail.com>
2021-04-25 01:11:10 -0700

    add parallax, images, about me

---
+++ scripts/zillow-api.js
@@ -0,0 +1,28 @@
+
+// zillow.com api
+// var getImg = () => {
+// 	return fetch("https://zillow-com.p.rapidapi.com/property/15162566/media", {
+// 		"method": "GET",
+// 		"headers": {
+// 			"x-rapidapi-host": "zillow-com.p.rapidapi.com",
+// 			"x-rapidapi-key": "d08f3333b1msh6316a06c4f648fcp1365ccjsnfe7ee30f1737"
+// 		}
+// 	}).then((response) => {
+// 		if(response.status === 200){
+// 			var data = response.json()
+// 			return data
+// 		} else {
+// 			throw new Error("Unable to show photo");
+// 		}
+// 	}).then((data) => {
+// 		return data
+// 	}).catch((error) => {
+// 		return error
+// 	})
+// }
+
+
+// var listingImg = document.getElementById("listing-1");
+// listingImg.innerHTML = getImg();
+
+
