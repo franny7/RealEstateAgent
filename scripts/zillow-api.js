2f89f23ce4df699dc292c571071fe78e8d236cb6
Francisco Martinez <panchetz7@gmail.com>
2021-05-03 23:53:03 -0700

    add menu dropdown

--- scripts/zillow-api.js
+++ scripts/zillow-api.js
@@ -1,28 +1,17 @@
+var getImg = () => {
+  fetch('https://zillow-com1.p.rapidapi.com/images?zpid=2080998890', {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': 'd08f3333b1msh6316a06c4f648fcp1365ccjsnfe7ee30f1737',
+      'x-rapidapi-host': 'zillow-com1.p.rapidapi.com',
+    },
+  })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
-// zillow.com api
-// var getImg = () => {
-// 	return fetch("https://zillow-com.p.rapidapi.com/property/15162566/media", {
-// 		"method": "GET",
-// 		"headers": {
-// 			"x-rapidapi-host": "zillow-com.p.rapidapi.com",
-// 			"x-rapidapi-key": "d08f3333b1msh6316a06c4f648fcp1365ccjsnfe7ee30f1737"
-// 		}
-// 	}).then((response) => {
-// 		if(response.status === 200){
-// 			var data = response.json()
-// 			return data
-// 		} else {
-// 			throw new Error("Unable to show photo");
-// 		}
-// 	}).then((data) => {
-// 		return data
-// 	}).catch((error) => {
-// 		return error
-// 	})
-// }
-
-
-// var listingImg = document.getElementById("listing-1");
-// listingImg.innerHTML = getImg();
-
-
+console.log(getImg());
