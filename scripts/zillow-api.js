d1dde719f478738441fc3bde4ccc9ec82c297074
Francisco Martinez <panchetz7@gmail.com>
2021-05-04 15:13:53 -0700

    adjust mobile menu css

--- scripts/zillow-api.js
+++ scripts/zillow-api.js
@@ -1,17 +1,17 @@
-var getImg = () => {
-  fetch('https://zillow-com1.p.rapidapi.com/images?zpid=2080998890', {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': 'd08f3333b1msh6316a06c4f648fcp1365ccjsnfe7ee30f1737',
-      'x-rapidapi-host': 'zillow-com1.p.rapidapi.com',
-    },
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
+// var getImg = () => {
+//   fetch('https://zillow-com1.p.rapidapi.com/images?zpid=2080998890', {
+//     method: 'GET',
+//     headers: {
+//       'x-rapidapi-key': 'd08f3333b1msh6316a06c4f648fcp1365ccjsnfe7ee30f1737',
+//       'x-rapidapi-host': 'zillow-com1.p.rapidapi.com',
+//     },
+//   })
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// };
 
-console.log(getImg());
+// console.log(getImg());
